0fa324c7238a0cb60549b67cef7ebc53e112f54b
MKhodagholi <mdkhodagholi@gmail.com>
2023-06-10 01:45:31 +0330

    remove console.log disabler in production

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 // replace console.* for disable log on production
-if (process.env.NODE_ENV === "production") {
-  console.log = () => {};
-  console.error = () => {};
-  console.debug = () => {};
-}
+// if (process.env.NODE_ENV === "production") {
+//   console.log = () => {};
+//   console.error = () => {};
+//   console.debug = () => {};
+// }
 
 const app = document.getElementById("app");
 
