033100696822728be1daa3c0d79f106bdf6a776a
MKhodagholi <mdkhodagholi@gmail.com>
2023-06-10 00:58:55 +0330

    init: project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+// replace console.* for disable log on production
+if (process.env.NODE_ENV === "production") {
+  console.log = () => {};
+  console.error = () => {};
+  console.debug = () => {};
+}
+
+const app = document.getElementById("app");
+
+const root = createRoot(app);
+
+root.render(<App />);
