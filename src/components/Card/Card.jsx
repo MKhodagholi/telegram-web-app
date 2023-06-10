7c69ea7ce8a62149fd82dd3d5c5de83d7daa2162
MKhodagholi <mdkhodagholi@gmail.com>
2023-06-10 15:13:47 +0330

    change

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -17,7 +17,7 @@ const Card = ({ card }) => {
     //     })
     //   );
 
-    if (tele.sendData) tele.sendData({ data });
+    if (tele.sendData) tele.sendData(JSON.stringify({ data }));
     tele.close();
   };
 
